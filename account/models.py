e0b3df5a0161ebc45deabd4beda48c5abcdb2e2d
vickypanchal501 <panchalvicky501@gmail.com>
2023-10-09 17:12:52 +0530

    second commit

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+import time 
+import uuid
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionManager
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
