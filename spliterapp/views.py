b46b5eceb1d8f909be1b4d9f78bb5ffe41fd0db6
vickypanchal501 <panchalvicky501@gmail.com>
2023-11-20 12:23:19 +0530

    fix the error in our code expense

--- spliterapp/views.py
+++ spliterapp/views.py
@@ -30,38 +30,40 @@ def group_detail(request, group_id):
             pass
           
     return render(request, 'group/base.html', {'group': group, 'user_groups': user_groups, 'user_id': user })
-
+# Your existing view code ...
 
 def add_expense(request, group_id):
     group = Group.objects.get(id=group_id)
     split_amount = 0 
-    # import pdb; pdb.set_trace()
+
     if request.method == 'POST':
-        form = ExpenseForm(group,request.POST)
+        form = ExpenseForm(group, request.POST)
         
         if form.is_valid():
             expense = form.save(commit=False)
             expense.created_by = request.user
             expense.group = group
-            expense.save()
 
-            # Calculate the split amount
-            split_with_users = form.cleaned_data['split_with']
+            # Get split amount from the form
             split_amount = form.cleaned_data['split_amount']
-
-            # Calculate the amount for each user
-            amount_per_user = split_amount / (len(split_with_users) + 1)
-            expense.split_amount = amount_per_user
+            
+            # Save the expense without calculating split amount for now
             expense.save()
 
             # Add the creator to the split_with users
-            split_with_users = list(split_with_users)
+            split_with_users = list(form.cleaned_data['split_with'])
             split_with_users.append(request.user)
             expense.split_with.set(split_with_users)
+
+            # Calculate the amount for each user
+            amount_per_user = split_amount / len(expense.split_with.all())
+            expense.split_amount = amount_per_user
+            expense.save()
 
             return redirect('group_detail', group_id=group.id)
     else:
         form = ExpenseForm(group)
+    
     context = {
         'group': group,
         'form': form,
